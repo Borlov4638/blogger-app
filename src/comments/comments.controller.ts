7d2ec7f60e35950c60c569e12185e2823d9a39cd
orisborlov <mrwiggle40000@gmail.com>
2023-10-02 20:58:38 +0700

    added fet comments endpoint

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Req } from '@nestjs/common';
 import { CommentsService } from './comments.service';
+import { Request } from 'express';
 
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
   @Get(':id')
-  async getCommentById(@Param('id') comId: string) {
-    return await this.commentsService.getCommentById(comId);
+  async getCommentById(@Param('id') comId: string, @Req() request: Request) {
+    return await this.commentsService.getCommentById(comId, request);
   }
 }
--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -2,18 +2,43 @@ import { NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import { Comment } from '../entyties/comments.schema';
+import { Request } from 'express';
+import { LikeStatus } from 'src/enums/like-status.enum';
+import { JwtService } from '@nestjs/jwt';
+interface IUsersAcessToken{
+  id:string
+  email:string
+  login:string
+}
 
 export class CommentsService {
   constructor(
     @InjectModel(Comment.name) private commentModel: Model<Comment>,
+    private jwtService :JwtService
   ) {}
-  async getCommentById(id: string) {
+  async getCommentById(id: string, request: Request) {
     const foundedComment = await this.commentModel.findById(
       new Types.ObjectId(id),
-    );
+      {__v:false, _id:false}
+    )
     if (!foundedComment) {
       throw new NotFoundException();
     }
-    return foundedComment;
+    
+    let token :string
+    if(request.headers.authorization){
+      token = request.headers.authorization.split(' ')[1]
+    }
+    
+      let myStatus = LikeStatus.NONE
+      if(token){
+          const user : IUsersAcessToken = this.jwtService.verify(token)
+          if(user){
+              myStatus = foundedComment.getLikeStatus(user.id)
+          }
+      }
+      const likesCount = foundedComment.likesInfo.usersWhoLiked.length
+      const dislikesCount = foundedComment.likesInfo.usersWhoDisliked.length
+      return {...(foundedComment.toObject()), likesInfo:{likesCount, dislikesCount, myStatus}}
   }
 }
