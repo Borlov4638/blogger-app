df66e1e4a5c270bac922cd6d0866f5b5a7bfcad3
orisborlov <mrwiggle40000@gmail.com>
2023-10-04 14:58:30 +0700

    delete fixed

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,8 @@ import { Blog } from './entyties/blogs.schema';
 import { Model } from 'mongoose';
 import { Post } from './entyties/posts.schema';
 import { User } from './entyties/users.chema';
+import { Comment } from './entyties/comments.schema';
+import { Session } from './entyties/session.schema';
 
 @Injectable()
 export class AppService {
@@ -11,11 +13,15 @@ export class AppService {
     @InjectModel(Blog.name) private blogsModel: Model<Blog>,
     @InjectModel(Post.name) private postModel: Model<Post>,
     @InjectModel(User.name) private userModel: Model<User>,
+    @InjectModel(Comment.name) private commentModel: Model<Comment>,
+    @InjectModel(Session.name) private sessionModel: Model<Session>
   ) {}
   async deleteAllData(): Promise<void> {
     await this.blogsModel.deleteMany({});
     await this.postModel.deleteMany({});
     await this.userModel.deleteMany({});
+    await this.commentModel.deleteMany({});
+    await this.sessionModel.deleteMany({})
     return;
   }
 }
