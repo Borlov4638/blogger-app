155d2e6c0b0861c8d3d0855e21ba8adcc44bdea3
orisborlov <mrwiggle40000@gmail.com>
2023-10-04 13:23:02 +0700

    fixed coocie-parser import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './custom-exceptions/exception-filter';
-import * as cookieParser from 'cookie-parser';
+const cookieParser = require('cookie-parser') 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
