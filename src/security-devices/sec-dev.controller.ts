971609fafa8d8fde8bfe2c23fe907ea23950f750
orisborlov <mrwiggle40000@gmail.com>
2023-10-09 19:37:40 +0700

    1

--- src/security-devices/sec-dev.controller.ts
+++ src/security-devices/sec-dev.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Delete, Get, Param, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import {
-  BearerAccessAuthGuard,
   BearerRefreshAuthGuard,
 } from '../auth/guards/auth.bearer.guard';
 import { SecDevService } from './sec-dev.service';
@@ -12,21 +11,21 @@ export class SecDevController {
   constructor(
     private readonly secDevService: SecDevService,
     private readonly sessionService: SessionService,
-  ) {}
-  @UseGuards(BearerAccessAuthGuard)
+  ) { }
+  @UseGuards(BearerRefreshAuthGuard)
   @Get('devices')
   async getUserDevices(@Req() request: Request) {
     await this.sessionService.validateSession(request);
     return await this.secDevService.getUserDevices(request);
   }
 
-  @UseGuards(BearerAccessAuthGuard)
+  @UseGuards(BearerRefreshAuthGuard)
   @Delete('devices')
   async deleteOtherSessions(@Req() request: Request) {
     await this.sessionService.validateSession(request);
     return await this.sessionService.deleteOtherSessions(request);
   }
-  @UseGuards(BearerAccessAuthGuard, BearerRefreshAuthGuard)
+  @UseGuards(BearerRefreshAuthGuard)
   @Delete('devices/:id')
   async deleteSessionById(@Req() request: Request, @Param('id') id: string) {
     await this.sessionService.validateSession(request);
