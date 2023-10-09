e23858a2905e3a46ca541ae2f4395237ce176478
orisborlov <mrwiggle40000@gmail.com>
2023-10-09 19:22:21 +0700

    -----

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,12 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
+
+  @Get()
+  startingPage() {
+    return "project is running"
+  }
 
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete('testing/all-data')
